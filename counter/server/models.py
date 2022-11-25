fb60613dbf2c69f532c0fe483061b96933bf92c4
Alexander <satrey.mail@gmail.com>
2022-11-25 06:48:36 +0500

    add models.py

--- counter/server/models.py
+++ counter/server/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Counter(models.Model):
-    counter = models.IntegerField(blank=False, null=True)
+    counter = models.IntegerField(blank=False, null=True, default=0)
 
     def __str__(self):
         return self.counter
